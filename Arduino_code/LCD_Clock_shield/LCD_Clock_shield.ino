99344aa220b38261dc374cea2149913357f70981
Stephen Zhang <x.stephen.zhang@gmail.com>
2022-06-08 14:57:00 -0400

    Update LCD_Clock_shield.ino

--- Arduino_code/LCD_Clock_shield/LCD_Clock_shield.ino
+++ Arduino_code/LCD_Clock_shield/LCD_Clock_shield.ino
@@ -50,10 +50,10 @@ void setup() {
   if (! rtc.initialized()) {
     Serial.println("RTC is NOT running!");
     // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
     // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+    rtc.adjust(DateTime(2022, 6, 8, 10, 49, 0));
   }
   
   // Initialize LCD.
